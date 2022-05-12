26f90663e8a622ef259b298826decca71dc71107
Saleh Sayeem <vdr_ssayeem@parata.com>
2022-05-12 20:00:01 +0600

    averagesalary

---
+++ AverageSalaryForEmployee.cs
@@ -0,0 +1,16 @@
+﻿namespace Leetcode
+{
+    public class AverageSalaryForEmployee
+    {
+        public double Average(int[] salary)
+        {
+            Array.Sort(salary);
+            double sum = 0;
+            for (int i = 1; i < salary.Length - 1; i++)
+            {
+                sum += salary[i];
+            }
+            return (sum) / (salary.Length - 2);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,19 @@
 ﻿
-using Leetcode;
-
 //MaxSubArrayFinding max = new MaxSubArrayFinding();
 //int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
 //var ans = max.MaxSubArray(nums);
 //Console.WriteLine(ans);
 
-CountOddsCalculation c = new CountOddsCalculation();
-int a = 3, b = 7;
-int ans = c.CountOdds(a, b);
-Console.WriteLine(ans);
+//CountOddsCalculation c = new CountOddsCalculation();
+//int a = 3, b = 7;
+//int ans = c.CountOdds(a, b);
+//Console.WriteLine(ans);
 
+using Leetcode;
+
+int[] arr = { 1000, 2000, 3000, 4000 };
+AverageSalaryForEmployee average = new AverageSalaryForEmployee();
+var ans = average.Average(arr);
+Console.WriteLine(ans);
 
 
